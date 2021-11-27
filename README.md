57e27203eac2ce756a3d780c3ac87be57b9cbb1f
samuelds <samuelds.pro@gmail.com>
2021-11-27 14:10:35 +0100

    Start config file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# maps-lib
+# maps
+
+## Online
+Online map : https://maps.foxhole.tools
+
+## License
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE](LICENSE) file for details
---
+++ dist/config/tileLayer.js
@@ -0,0 +1,177 @@
+const tileUrl = 'https://assets.foxhole.tools/tile/clasic';
+
+const mapListConf = [
+
+    {
+        name: 'Acrithia',
+        tile: 'acrithia'
+    },
+    {
+        name: 'Allod\'s Bight',
+        tile: 'allodsbight'
+    },
+    {
+        name: 'Ash Fields',
+        tile: 'ashfields'
+    },
+    {
+        name: 'Basin Sionnach',
+        tile: 'basinsionnach'
+    },
+    {
+        name: 'Callahans Passage',
+        tile: 'callahanspassage'
+    },
+    {
+        name: 'Callums Cape',
+        tile: 'callumscape'
+    },
+    {
+        name: 'Clanshead Valley',
+        tile: 'clansheadvalley'
+    },
+    {
+        name: 'Deadlands',
+        tile: 'deadlands'
+    },
+    {
+        name: 'The Drowned Vale',
+        tile: 'drownedvale'
+    },
+    {
+        name: 'Endless Shore',
+        tile: 'endlessshore'
+    },
+    {
+        name: 'Farranac Coast',
+        tile: 'farranaccoast'
+    },
+    {
+        name: 'Fisherman\'s Row',
+        tile: 'fishermansrow'
+    },
+    {
+        name: 'Godcrofts',
+        tile: 'godcrofts'
+    },
+    {
+        name: 'Great March',
+        tile: 'greatmarch'
+    },
+    {
+        name: 'The Heartlands',
+        tile: 'heartlands'
+    },
+    {
+        name: 'Howl County',
+        tile: 'howlcounty'
+    },
+    {
+        name: 'Kalokai',
+        tile: 'kalokai'
+    },
+    {
+        name: 'The Linn of Mercy',
+        tile: 'linnmercy'
+    },
+    {
+        name: 'Loch Mór',
+        tile: 'lochmor'
+    },
+    {
+        name: 'Marban Hollow',
+        tile: 'marbanhollow'
+    },
+    {
+        name: 'Mooring County',
+        tile: 'mooringcounty'
+    },
+    {
+        name: 'Morgens Crossing',
+        tile: 'morgenscrossing'
+    },
+    {
+        name: 'Nevish Line',
+        tile: 'nevishline'
+    },
+    {
+        name: 'The Oarbreaker Isles',
+        tile: 'oarbreaker'
+    },
+    {
+        name: 'Origin',
+        tile: 'origin'
+    },
+    {
+        name: 'Reaching Trail',
+        tile: 'reachingtrail'
+    },
+    {
+        name: 'Red River',
+        tile: 'redriver'
+    },
+    {
+        name: 'Shackled Chasm',
+        tile: 'shackledchasm'
+    },
+    {
+        name: 'Speaking Woods',
+        tile: 'speakingwoods'
+    },
+    {
+        name: 'Stonecradle',
+        tile: 'stonecradle'
+    },
+    {
+        name: 'Tempest Island',
+        tile: 'tempestisland'
+    },
+    {
+        name: 'Terminus',
+        tile: 'terminus'
+    },
+    {
+        name: 'The fingers',
+        tile: 'thefingers'
+    },
+    {
+        name: 'Umbral Wildwood',
+        tile: 'umbralwildwood'
+    },
+    {
+        name: 'Viper Pit',
+        tile: 'viperpit'
+    },
+    {
+        name: 'Weathered Expanse',
+        tile: 'weatheredexpanse'
+    },
+    {
+        name: 'Westgate',
+        tile: 'westgate'
+    },
+
+    // Homes
+    {
+        name: 'Home colonial',
+        tile: 'homeregionc'
+    },
+    {
+        name: 'Home warden',
+        tile: 'homeregionw'
+    },
+
+    // Global
+    {
+        name: 'Global map',
+        tile: 'warmap'
+    },
+
+];
+
+// Set all tile
+// let mapList = {};
+// for (const map of mapListConf)
+// {
+//     mapList{map} = L.tileLayer(tileUrl + map + '/{z}/{z}_{x}_{y}.png');
+// }
